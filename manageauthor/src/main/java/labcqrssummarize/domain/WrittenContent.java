0744651b537065af82d3491557da38b116994815
jihyun <lifeisggv@gmail.com>
2025-06-30 12:01:10 +0900

    작가관리 바운디드 컨텍스트

--- manageauthor/src/main/java/labcqrssummarize/domain/WrittenContent.java
+++ manageauthor/src/main/java/labcqrssummarize/domain/WrittenContent.java
@@ -11,15 +11,13 @@ import lombok.*;
 @ToString
 public class WrittenContent extends AbstractEvent {
 
-    private Long id;
+    private Stirng ebookId;
     private String authorId;
     private Boolean isApproved;
 
     public WrittenContent(Author aggregate) {
         super(aggregate);
-        this.id = aggregate.getId();
-        this.authorId = aggregate.getauthorId();
-        this.isApproved = aggregate.getisApproved();
+        this.id = aggregate.getEbookId();
     }
 
     public WrittenContent() {
